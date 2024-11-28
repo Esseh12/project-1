3e99868cf85c810eae7f380c74c094ae8b377c26
Esseh12 <gbenmoese788@gmail.com>
2024-11-28 11:39:37 +0100

    app.jsx

--- bank-project/src/main.jsx
+++ bank-project/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-import App from "./app";
+import App from "./app.jsx";
 import "./main.css";
 
 createRoot(document.getElementById("root")).render(
