dd64ec7eb1b1de5ac84f22479b1dd8a4439c48b1
Esseh12 <gbenmoese788@gmail.com>
2024-11-28 18:11:32 +0100

    fix: enable loading of Modernizr script and dynamically load external scripts in Header component

--- bank-project/src/components/Header.jsx
+++ bank-project/src/components/Header.jsx
@@ -2,47 +2,48 @@
 import PropTypes from "prop-types";
 import DesktopMenu from "./DesktopMenu";
 import MobileMenu from "./MobileMenu";
+import { useEffect } from "react";
 
 const Header = ({ children }) => {
-  // useEffect(() => {
-  //   // External scripts to be loaded dynamically
-  //   const scripts = [
-  //     "/ResourcePackages/MCBMU/assets/dist/vendor/plugins/js/plugins.min7306.js",
-  //     "/ResourcePackages/MCBMU/assets/dist/js/theme7306.js",
-  //     "/ResourcePackages/MCBMU/assets/dist/js/views/view.contact7306.js",
-  //     "/ResourcePackages/MCBMU/assets/dist/js/demos/demo-business-consulting-27306.js",
-  //     "/ResourcePackages/MCBMU/assets/dist/js/custom7306.js",
-  //     "/ResourcePackages/MCBMU/assets/dist/js/ma5-menu7306.js",
-  //     "/ResourcePackages/MCBMU/assets/dist/js/theme.init7306.js",
-  //     "/ResourcePackages/MCBMU/assets/dist/js/bankgurantee7306.js",
-  //     "/ResourcePackages/MCB/assets/dist/js/anychart/anychart-ui.min7306.js",
-  //     "/ResourcePackages/MCB/assets/dist/js/anychart/anychart-stock.min7306.js",
-  //     "/ResourcePackages/MCBMU/assets/dist/js/forexchartData7306.js",
-  //     "/ResourcePackages/MCBMU/assets/dist/js/forexchart7306.js",
-  //     "/ResourcePackages/MCBMU/assets/dist/js/iban7306.js",
-  //     "ResourcePackages/MCBMU/assets/dist/js/forexdata7306.js",
-  //     "ResourcePackages/MCBMU/assets/dist/js/forexCalculator7306.js",
-  //     "ResourcePackages/MCBMU/assets/dist/js/forexCalculator7306.js",
-  //   ];
+  useEffect(() => {
+    // External scripts to be loaded dynamically
+    const scripts = [
+      "../../public/ResourcePackages/MCBMU/assets/dist/vendor/plugins/js/plugins.min7306.js",
+      "/ResourcePackages/MCBMU/assets/dist/js/theme7306.js",
+      "/ResourcePackages/MCBMU/assets/dist/js/views/view.contact7306.js",
+      "/ResourcePackages/MCBMU/assets/dist/js/demos/demo-business-consulting-27306.js",
+      "/ResourcePackages/MCBMU/assets/dist/js/custom7306.js",
+      "/ResourcePackages/MCBMU/assets/dist/js/ma5-menu7306.js",
+      "/ResourcePackages/MCBMU/assets/dist/js/theme.init7306.js",
+      "/ResourcePackages/MCBMU/assets/dist/js/bankgurantee7306.js",
+      "/ResourcePackages/MCB/assets/dist/js/anychart/anychart-ui.min7306.js",
+      "/ResourcePackages/MCB/assets/dist/js/anychart/anychart-stock.min7306.js",
+      "/ResourcePackages/MCBMU/assets/dist/js/forexchartData7306.js",
+      "/ResourcePackages/MCBMU/assets/dist/js/forexchart7306.js",
+      "/ResourcePackages/MCBMU/assets/dist/js/iban7306.js",
+      "ResourcePackages/MCBMU/assets/dist/js/forexdata7306.js",
+      "ResourcePackages/MCBMU/assets/dist/js/forexCalculator7306.js",
+      "ResourcePackages/MCBMU/assets/dist/js/forexCalculator7306.js",
+    ];
 
-  //   scripts.forEach((src) => {
-  //     const script = document.createElement("script");
-  //     script.src = src;
-  //     script.async = false;
-  //     script.onerror = () => console.error(`Failed to load script: ${src}`);
-  //     document.body.appendChild(script);
-  //   });
+    scripts.forEach((src) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.async = false;
+      script.onerror = () => console.error(`Failed to load script: ${src}`);
+      document.body.appendChild(script);
+    });
 
-  //   // Cleanup scripts on component unmount
-  //   return () => {
-  //     scripts.forEach((src) => {
-  //       const script = document.querySelector(`script[src="${src}"]`);
-  //       if (script && script.parentNode === document.body) {
-  //         document.body.removeChild(script);
-  //       }
-  //     });
-  //   };
-  // }, []);
+    // Cleanup scripts on component unmount
+    return () => {
+      scripts.forEach((src) => {
+        const script = document.querySelector(`script[src="${src}"]`);
+        if (script && script.parentNode === document.body) {
+          document.body.removeChild(script);
+        }
+      });
+    };
+  }, []);
 
   return (
     <header id="header">
